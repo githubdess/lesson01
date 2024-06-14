f36ea159527abc5a0496eeb30727dd3b48b3c0ed
githubdess <dess_81@mail.ru>
2024-06-14 12:07:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+a = '0, 1, 1, 2, 3, 5, 8, 13, 21, 34'
+b = len(a)
+print(b)
+
+first = 666
+second = 13
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+first, second, third = 2, 5, 7
+mean = (first + second + third) / 3
+print(mean)
+
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string + ", " + first_string)
+
+a = 3.14
+b = 79
+c = 2.718
+f = (a * b) + (a * c)
+d = f ** 3 / 2
+print(d)
